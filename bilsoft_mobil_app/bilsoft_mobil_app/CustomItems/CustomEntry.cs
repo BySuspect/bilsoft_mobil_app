38778c0ee98fc7cbdd6077c9a0a4a3c1b066298f
BySuspect <omerbayraktar43@gmail.com>
2022-08-08 10:33:33 +0300

    customEntry Deişti

--- bilsoft_mobil_app/bilsoft_mobil_app/CustomItems/CustomEntry.cs
+++ bilsoft_mobil_app/bilsoft_mobil_app/CustomItems/CustomEntry.cs
@@ -3,63 +3,172 @@ using Xamarin.Forms;
 
 namespace bilsoft_mobil_app.CustomItems
 {
-    [Obsolete]
-    public class CustomEntry : Entry
+    public class CustomEntry : Frame
     {
-        public static readonly BindableProperty BorderColorProperty =
-        BindableProperty.Create(
-            nameof(BorderColor),
-            typeof(Color),
-            typeof(CustomEntry),
-            Color.Gray);
+        BorderlessEntry _entry;
 
-        // Gets or sets BorderColor value
-        public Color BorderColor
+        string _text = "",
+               _placeholder = "";
+
+        TextAlignment _horizontal = TextAlignment.Center,
+                      _vertical = TextAlignment.Center;
+
+        int _entryFontSize = 20;
+
+        Keyboard _entryKeyboard;
+
+        Color _entryTextColor = Color.Black,
+              _placeholderColor = Color.Default;
+        public CustomEntry() : base()
         {
-            get { return (Color)GetValue(BorderColorProperty); }
-            set { SetValue(BorderColorProperty, value); }
+            _entry = new BorderlessEntry();
+            #region Entry Settings
+            _entry.Margin = new Thickness(-15);
+            _entry.TextColor = _entryTextColor;
+            _entry.FontSize = _entryFontSize;
+            _entry.SetBinding(Entry.TextProperty, "Value");
+            _entry.Keyboard = _entryKeyboard;
+            _entry.Text = _text;
+            _entry.Placeholder = _placeholder;
+            _entry.PlaceholderColor = _placeholderColor;
+            _entry.HorizontalTextAlignment = _horizontal;
+            _entry.VerticalTextAlignment = _vertical;
+            _entry.TextChanged += (sender, args) =>
+            {
+                OnTextChanged(args);
+            };
+            _entry.Unfocused += (sender, args) =>
+            {
+                OnUnFocused(args);
+            };
+            #endregion
+            this.Content = _entry;
+        }
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                _entry.Text = value;
+            }
+
+        }
+        public TextAlignment HorizontalTextAlignment
+        {
+            get
+            {
+                return _horizontal;
+            }
+            set
+            {
+                _horizontal = value;
+                _entry.HorizontalTextAlignment = value;
+
+            }
+        }
+        public TextAlignment VerticalTextAlignment
+        {
+            get
+            {
+                return _vertical;
+            }
+            set
+            {
+                _vertical = value;
+                _entry.VerticalTextAlignment = value;
+            }
+        }
+        public string Placeholder
+        {
+            get
+            {
+                return _placeholder;
+            }
+            set
+            {
+                _placeholder = value;
+                _entry.Placeholder = value;
+            }
+        }
+        public Keyboard Keyboard
+        {
+            get
+            {
+                return _entryKeyboard;
+            }
+            set
+            {
+                _entryKeyboard = value;
+                _entry.Keyboard = value;
+            }
+        }
+        public Color TextColor
+        {
+            get
+            {
+                return _entryTextColor;
+            }
+            set
+            {
+                _entry.TextColor = value;
+                _entryTextColor = value;
+            }
+        }
+        public Color PlaceholderColor
+        {
+            get
+            {
+                return _placeholderColor;
+            }
+            set
+            {
+                _entry.PlaceholderColor = value;
+                _placeholderColor = value;
+            }
+        }
+        public int FontSize
+        {
+            get
+            {
+                return _entryFontSize;
+            }
+            set
+            {
+                _entry.FontSize = value;
+                _entryFontSize = value;
+            }
         }
 
-        public static readonly BindableProperty BorderWidthProperty =
-        BindableProperty.Create(
-            nameof(BorderWidth),
-            typeof(int),
-            typeof(CustomEntry),
-            Device.OnPlatform<int>(1, 2, 2));
-
-        // Gets or sets BorderWidth value
-        public int BorderWidth
+        public int TextFontSize
         {
-            get { return (int)GetValue(BorderWidthProperty); }
-            set { SetValue(BorderWidthProperty, value); }
+            get
+            {
+                return _entryFontSize;
+            }
+            set
+            {
+                _entry.FontSize = value;
+                _entry.FontSize = value;
+                _entryFontSize = value;
+            }
         }
 
-        public static readonly BindableProperty CornerRadiusProperty =
-        BindableProperty.Create(
-            nameof(CornerRadius),
-            typeof(double),
-            typeof(CustomEntry),
-            Device.OnPlatform<double>(6, 7, 7));
-
-        // Gets or sets CornerRadius value
-        public double CornerRadius
+        public event EventHandler<TextChangedEventArgs> TextChanged;
+        protected virtual void OnTextChanged(TextChangedEventArgs e)
         {
-            get { return (double)GetValue(CornerRadiusProperty); }
-            set { SetValue(CornerRadiusProperty, value); }
+            EventHandler<TextChangedEventArgs> handler = TextChanged;
+            handler?.Invoke(this, e);
         }
 
-        public static readonly BindableProperty IsCurvedCornersEnabledProperty =
-        BindableProperty.Create(
-            nameof(IsCurvedCornersEnabled),
-            typeof(bool),
-            typeof(CustomEntry),
-            true);
-
-        // Gets or sets IsCurvedCornersEnabled value
-        public bool IsCurvedCornersEnabled
+        public event EventHandler<FocusEventArgs> UnFocused;
+        protected virtual void OnUnFocused(FocusEventArgs e)
         {
-            get { return (bool)GetValue(IsCurvedCornersEnabledProperty); }
-            set { SetValue(IsCurvedCornersEnabledProperty, value); }
+            EventHandler<FocusEventArgs> handler = UnFocused;
+            handler?.Invoke(this, e);
         }
     }
 }
