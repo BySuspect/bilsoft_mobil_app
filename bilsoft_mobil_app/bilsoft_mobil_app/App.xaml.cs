04446aa37797ecd82a21c511f164220d6f896e31
BySuspect <omerbayraktar43@gmail.com>
2022-08-10 15:42:53 +0300

    10.08.2022

--- bilsoft_mobil_app/bilsoft_mobil_app/App.xaml.cs
+++ bilsoft_mobil_app/bilsoft_mobil_app/App.xaml.cs
@@ -18,14 +18,15 @@ namespace bilsoft_mobil_app
             InitializeComponent();
 
             /*
+             * loginde girişte uygulama çöküyor bakılacak
              * cariyi incele kısmı yapılıcak
              * ana sayfadaki hatırlatma düzenlenicek ana sayfa tekrar yapılıcak
              */
 
             //Test Verileri
-            /*APIHelper.loginToken = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjI4OCIsInVuaXF1ZV9uYW1lIjoiZDAzMDM1OGYtYmUwOC00YTEzLTlhMjQtMjZhM2ZhYzQzZTY5IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImRlbW8iLCJuYmYiOjE2NjAxMTQ4MjUsImV4cCI6MTY2MDE1ODAyNCwiaXNzIjoid3d3LmJpbHNvZnQuY29tIiwiYXVkIjoid3d3LmJpbHNvZnQuY29tIn0.8sXOnaK9gWdO-L-3qhAKZCb9roS4OQpns6PurfZ-J5o";
+            APIHelper.loginToken = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjI4OCIsInVuaXF1ZV9uYW1lIjoiZDAzMDM1OGYtYmUwOC00YTEzLTlhMjQtMjZhM2ZhYzQzZTY5IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImRlbW8iLCJuYmYiOjE2NjAxMTQ4MjUsImV4cCI6MTY2MDE1ODAyNCwiaXNzIjoid3d3LmJpbHNvZnQuY29tIiwiYXVkIjoid3d3LmJpbHNvZnQuY29tIn0.8sXOnaK9gWdO-L-3qhAKZCb9roS4OQpns6PurfZ-J5o";
             APIHelper.kullaniciAdi = "demo";
-            APIHelper.subeAd = "merkez";*/
+            APIHelper.subeAd = "merkez";
             //Test Verileri End
 
             //Giriş
